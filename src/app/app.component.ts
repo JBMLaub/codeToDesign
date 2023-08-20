e89d7e7441143a88779cf16c086e94d164bfe282
JBMLaub <jcblaub@gmail.com>
2023-08-20 18:00:04 +0200

    clean up structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,17 @@ import { Component, ElementRef, HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'designToCode';
-  //navButtons
+
+  // top buttons
   isActive = 'Typing'
   navButtons = ["Forms", "Edit", "Typing", "Positioning", "Add Ins", "User"]
   selectedButton(button: any) {
     this.isActive = button
   }
-  //littleForm
-  littleFormSelection = 'Times New Roman'
-  littleFormWindow = 'Sans Serif'
-  littleFormFonts = ['Sans Serif', 'Montserrat', 'Arial Narrow', "Courier New"]
-
-  //Counter
-  counterWindow = ['5', '8', '10', '11', '12', '13', '14', '15', '16']
-
-  //ngx-color-picker-typing
-  color = 'orange'
-  colours = []
+  //-------------------------------------------
 
 
-  //drag and drop 
+  // forms
   el: any = undefined
   handlers: any = undefined
   dragState = 'not dragging'
@@ -36,21 +27,19 @@ export class AppComponent {
       this.el = document.getElementsByClassName('formSquare')[0]
       let screen = document.getElementsByClassName('screen')[0]
       screen.insertBefore(this.el, screen.firstElementChild);
-      this.el.style.position = 'absolute'
       this.el.style.boxSizing = 'border-box'
+      this.el.style.position = 'absolute'
       this.dragState = 'is dragging'
-      this.activateForm()
     } else if (this.dragState === 'is dragging') {
       this.dragState = 'not dragging'
     }
     if (e.target.className === 'formSquare__handler--topLeft') {
       this.changeSize = 'isResizing'
-
-      console.log(e.target.clientX + 'px', 'is client X')
     }
   }
   @HostListener('mousemove', ['$event'])
   mousemove(e: any) {
+    if (this.el?.style.top.split('px')[0] > 110) { }
     if (this.dragState === 'is dragging') {
       this.el.style.top = e.clientY + 'px'
       this.el.style.left = e.clientX + 'px  '
@@ -60,29 +49,10 @@ export class AppComponent {
       this.el.style.width = this.el.style.width.split('px')[0] + e.clientX + 'px'
     }
   }
-
-
-  makeFullWidth() {
-    this.el.style.left = '0px'
-    this.el.style.width = '100%'
-  }
-  //ngx-color-picker-shapes
-  shapeColour = 'orange'
-  shapeColours = []
-  cpEdit: any
-  changeBackgroundColour() {
-    this.el = document.getElementsByClassName('formSquare')[0]
-    this.el.style.backgroundColor = this.shapeColour
-    this.cpEdit = document.getElementsByClassName('formSquare')[0]
-    this.cpEdit.addEventListener('click', function () { console.log('cpEdit') })
-    this.el.addEventListener('input')
-  }
-
   changeSize: any
   activeShape: any
   handlersAdded: any = 'notAdded'
   activateForm() {
-    //add handlers
     if (this.handlersAdded === 'notAdded') {
       this.el = document.getElementsByClassName('formSquare')[0]
       const topLeft = document.createElement("div");
@@ -94,9 +64,42 @@ export class AppComponent {
       this.handlersAdded = 'areAdded'
 
     }
-    //adapt border
-    //update activeItem Var
   }
+  //--------------------------------------------
+
+  // edit
+  makeFullWidth() {
+    this.el.style.left = '0px'
+    this.el.style.width = '100%'
+  }
+
+  shapeColour = 'orange'
+  shapeColours = []
+  cpEdit: any
+  changeBackgroundColour() {
+    this.el = document.getElementsByClassName('formSquare')[0]
+    this.el.style.backgroundColor = this.shapeColour
+    this.cpEdit = document.getElementsByClassName('formSquare')[0]
+    this.cpEdit.addEventListener('click', function () { console.log('cpEdit') })
+    this.el.addEventListener('input')
+  }
+  //--------------------------------------
+
+  // typing
+  littleFormSelection = 'Times New Roman'
+  littleFormWindow = 'Sans Serif'
+  littleFormFonts = ['Sans Serif', 'Montserrat', 'Arial Narrow', "Courier New"]
+
+  counterWindow = ['5', '8', '10', '11', '12', '13', '14', '15', '16']
+
+  color = 'orange'
+  colours = []
+  //----------------------------------------
+
+  // positioning
+  // addins
+  // user
+
 
 
 }
